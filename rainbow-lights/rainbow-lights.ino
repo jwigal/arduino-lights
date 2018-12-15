e07ab7e377b04768e58c59b261d7fcaa032f2e58
Jeff Wigal <jeff@bluezebrasports.com>
2018-12-15 16:54:52 -0500

    fix sudden shift of colors at the end of the offset loop

--- rainbow-lights/rainbow-lights.ino
+++ rainbow-lights/rainbow-lights.ino
@@ -1,22 +1,30 @@
+/*
+  Rainbow Lights 
+  Jeff Wigal, December 2018
+
+  Repeat the full spectrum of colors every 255 lights on a string. 
+  Shift colors forward 1 light every second. 
+  For shorter strings, change COMPRESSION to something bigger than 1
+  
+*/
 #include <FastLED.h>
 #define NUM_LEDS 300
 #define LED_PIN 6
 #define LED_TYPE WS2811
 #define COLOR_ORDER RGB
-
+#define COMPRESSION 1
 CRGB leds[NUM_LEDS];
 
 void setup() {
-  delay(3000); // sanity delay
+  delay(3000);
   FastLED.addLeds<LED_TYPE,LED_PIN,COLOR_ORDER>(leds,NUM_LEDS);
 }
 
 void loop() 
 {
-
-    for(int offset = 0 ; offset < NUM_LEDS ; offset++){
+    for(int offset = 0 ; offset < 255 ; offset++){
       for(int i = 0; i < NUM_LEDS; i++ ) {
-        CRGB color = CHSV((i + offset) % 255,255,255);
+        CRGB color = CHSV(((i + offset) * COMPRESSION) % 255,255,255);
         leds[i] = color;
       }
       FastLED.show();
